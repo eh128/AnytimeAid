be93f8398d6b018e0d01f911194bf1603f602260
eh128 <ethanhsu128@gmail.com>
2022-08-20 20:24:27 -0700

    location

--- Pages/MapPage/MapPage.js
+++ Pages/MapPage/MapPage.js
@@ -1,11 +1,18 @@
-import { StyleSheet, View, Dimensions, ActivityIndicator } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 import NavBar from "../../Components/NavBar";
 import React, { useState, useEffect } from "react";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import * as Location from "expo-location";
 
 export default function MapPage({ navigation }) {
   const [location, setLocation] = useState(null);
+  const [address, setAddress] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -16,16 +23,30 @@ export default function MapPage({ navigation }) {
       }
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+      let where = (await Location.reverseGeocodeAsync(location.coords))[0];
+      console.log(where);
+      setAddress(
+        where["name"] +
+          ", " +
+          where["city"] +
+          ", " +
+          where["region"] +
+          ", " +
+          where["country"]
+      );
     })();
   }, []);
+
   if (!location) {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <ActivityIndicator size="large" />
+
         <NavBar navigation={navigation} />
       </View>
     );
   }
+
   return (
     <View style={styles.container}>
       <MapView
@@ -37,7 +58,23 @@ export default function MapPage({ navigation }) {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+      >
+        <Marker
+          coordinate={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }}
+        />
+      </MapView>
+      {address ? (
+        <View style={styles.addressContainer}>
+          <Text style={styles.header}>Your Location</Text>
+          <Text style={styles.text}>{address}</Text>
+        </View>
+      ) : (
+        ""
+      )}
+
       <NavBar navigation={navigation} />
     </View>
   );
@@ -53,5 +90,25 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
+  },
+  addressContainer: {
+    position: "absolute",
+    bottom: 160,
+    backgroundColor: "pink",
+    width: "70%",
+    height: "16%",
+    borderRadius: 15,
+    alignItems: "center",
+    padding: 20,
+  },
+  header: {
+    fontWeight: "bold",
+    fontSize: 22,
+  },
+  text: {
+    fontSize: 18,
+    textAlign: "center",
+    width: "80%",
+    marginTop: 20,
   },
 });
